d72b867bce5fbf425becd3549aa4737b97286477
James Cuzella <james.cuzella@lyraphase.com>
2023-09-12 13:23:52 -0600

    Example: Matching with `Option<T>`

--- src/main.rs
+++ src/main.rs
@@ -45,6 +45,12 @@ fn main() {
 
     // Patterns That Bind to Values
     match_control_flow_patterns();
+
+    // Matching with `Option<T>`
+    matching_with_option_t();
+
+    // Matches are Exhaustive
+    plus_one_broken(Some(1));
 }
 
 /// # Defining an Enum
@@ -566,5 +572,80 @@ fn value_in_cents_state_quarters(coin: &Coin2) -> u8 {
             println!("State quarter from {:?}!", state);
             25
         }
+    }
+}
+
+/// # Matching with `Option<T>`
+///
+/// In the previous section, we wanted to get the inner `T` value out of the
+/// `Some` case when using `Option<T>`; we can also handle `Option<T>` using
+/// `match`, as we did with the `Coin` enum! Instead of comparing coins, we’ll
+/// compare the variants of `Option<T>`, but the way the `match` expression
+/// works remains the same.
+fn matching_with_option_t() {
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    println!("`five` is {:?}", five);
+    println!("`six` is {:?}", six);
+    println!("`none` is {:?}", none);
+}
+
+/// # Function to match an `Option<T>`
+///
+/// A function that uses a `match` expression on an `Option<i32>`
+///
+/// Let’s say we want to write a function that takes an `Option<i32>` and, if
+/// there’s a value inside, adds 1 to that value. If there isn’t a value inside,
+/// the function should return the `None` value and not attempt to perform any
+/// operations.
+///
+/// This function is very easy to write, thanks to `match`, and will look like
+/// `plus_one()`.
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
+/// # Matches Are Exhaustive
+///
+/// There’s one other aspect of `match` we need to discuss: the arms’ patterns
+/// must cover all possibilities. Consider this version of our `plus_one`
+/// function, which has a bug and won’t compile: We didn’t handle the `None`
+/// case, so this code will cause a bug. Luckily, it’s a bug Rust knows how to
+/// catch. If we try to compile this code, we’ll get this error:
+///
+/// ```bash
+/// $ cargo run
+///   Compiling enums v0.1.0 (file:///projects/enums)
+///   error[E0004]: non-exhaustive patterns: `None` not covered
+///    --> src/main.rs:3:15
+///     |
+///   3 |         match x {
+///     |               ^ pattern `None` not covered
+///     |
+///   note: `Option<i32>` defined here
+///    --> /rustc/d5a82bbd26e1ad8b7401f6a718a9c57c96905483/library/core/src/option.rs:518:1
+///     |
+///     = note:
+///   /rustc/d5a82bbd26e1ad8b7401f6a718a9c57c96905483/library/core/src/option.rs:522:5: not covered
+///     = note: the matched value is of type `Option<i32>`
+///   help: ensure that all possible cases are being handled by adding a match arm with a wildcard pattern or an explicit pattern as shown
+///     |
+///   4 ~             Some(i) => Some(i + 1),
+///   5 ~             None => todo!(),
+///     |
+///   
+///   For more information about this error, try `rustc --explain E0004`.
+///   error: could not compile `enums` due to previous error
+/// ```
+///
+fn plus_one_broken(x: Option<i32>) -> Option<i32> {
+    match x {
+        Some(i) => Some(i + 1),
+        None => todo!(), // If this is commented out, we get Compile Error!
     }
 }
